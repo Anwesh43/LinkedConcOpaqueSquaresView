d38b8f7a1779ae49ad62efbf5369dedc65800953
Anwesh43 <anweshthecool0@gmail.com>
2019-03-01 10:24:24 +0530

    adding Int and Float extension methods

--- concopaquesquaresview/src/main/java/com/anwesh/uiprojects/concopaquesquaresview/ConcOpaqueSquaresView.kt
+++ concopaquesquaresview/src/main/java/com/anwesh/uiprojects/concopaquesquaresview/ConcOpaqueSquaresView.kt
@@ -23,3 +23,10 @@ val strokeFactor : Int = 90
 val strokeColor : Int = Color.parseColor("#212121")
 val fillColor : Int = Color.parseColor("#4527A0")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i / n)
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
