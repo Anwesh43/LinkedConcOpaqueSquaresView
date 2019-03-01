3be1c36005068efbd79d7e9e2311b20f9a1d21f1
Anwesh43 <anweshthecool0@gmail.com>
2019-03-01 10:11:12 +0530

    adding libraries and constants

--- concopaquesquaresview/src/main/java/com/anwesh/uiprojects/concopaquesquaresview/ConcOpaqueSquaresView.kt
+++ concopaquesquaresview/src/main/java/com/anwesh/uiprojects/concopaquesquaresview/ConcOpaqueSquaresView.kt
@@ -4,3 +4,22 @@ package com.anwesh.uiprojects.concopaquesquaresview
  * Created by anweshmishra on 01/03/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val squares : Int = 4
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val strokeColor : Int = Color.parseColor("#212121")
+val fillColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
