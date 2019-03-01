2e75b64ad8e81e617ffe5ede181e9b9147b4afd2
Anwesh43 <anweshthecool0@gmail.com>
2019-03-01 10:33:19 +0530

    adding drawCOSNode canvas extension method

--- concopaquesquaresview/src/main/java/com/anwesh/uiprojects/concopaquesquaresview/ConcOpaqueSquaresView.kt
+++ concopaquesquaresview/src/main/java/com/anwesh/uiprojects/concopaquesquaresview/ConcOpaqueSquaresView.kt
@@ -15,7 +15,6 @@ import android.graphics.Color
 
 val nodes : Int = 5
 val squares : Int = 4
-val lines : Int = 4
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val sizeFactor : Float = 2.8f
@@ -29,4 +28,31 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i / n)
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawCOSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sizeGap : Float = size / squares
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    for (j in 0..(squares - 1)) {
+        val k : Int = squares - 1 - j
+        val efSize : Float = sizeGap * (k + 1)
+        val sc : Float = sc1.divideScale(k, squares)
+        val rect : RectF = RectF(-efSize, -efSize, efSize, efSize)
+        save()
+        paint.alpha = (255 * sc).toInt()
+        paint.style = Paint.Style.FILL
+        paint.color = fillColor
+        drawRect(rect, paint)
+        paint.style = Paint.Style.STROKE
+        paint.color = strokeColor
+        drawRect(rect, paint)
+        restore()
+    }
+}
